68242b43a39aed7486c49a94d5009d1dee1e8008
JGWill Gaia.WSL <jgi@jgwill.com>
2023-12-05 14:01:13 -0500

    rename

---
+++ _redist_prep.sh
@@ -0,0 +1,12 @@
+#find $(pwd) -type f -exec sed -i 's/jgwill\/devops-vscode-openai-developer/jgwill\/devops-vscode-openai-developer/g' {} +
+#find $(pwd) -type f -exec sed -i 's/jgwill\/Open-AI-Code-Helper/jgwill\/jgwilliahelper/g' {} +
+
+#find $(pwd) -type f -exec sed -i 's/devops-vscode-openai-developer/devops-vscode-openai-developer/g' {} +
+
+#find $(pwd) -type f -exec sed -i 's/devops-vscode-openai-developer/devops-vscode-openai-developer/g' {} +
+#find $(pwd) -type f -exec sed -i 's/IvanGrigorov/jgwill/g' {} +
+
+#find $(pwd) -type f -exec sed -i 's/jgwilliahelper/jgwilliahelper/g' {} +
+
+#find $(pwd) -type f -exec sed -i 's/JGWill Open AI helper/JGWill Open AI helper/g' {} +
+find $(pwd) -type f -exec sed -i 's/Open AI helper/JGWillIA helper/g' {} +
